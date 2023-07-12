f82a74f74fb349fedb4ef30efee408cec65a7e5a
JotaPrimo <gestald118@gmail.com>
2023-07-11 22:04:31 -0400

    tela de departments  ok

---
+++ SallesWebMvc/Controllers/DepartmentsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SallesWebMvc.Models;
+using System.Collections.Generic;
+
+namespace SallesWebMvc.Controllers
+{
+    public class DepartmentsController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Department> List = new List<Department>();
+            List.Add(new Department { Id = 1, Name = "Moveis" });
+
+            return View(List);
+        }
+    }
+}
